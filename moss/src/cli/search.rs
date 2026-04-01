08ba772678fb13241c17f0b82af39aec0203d46d
Jonathan Lopez <jonathanglopez@gmail.com>
2026-04-01 08:39:34 -0400

    moss/search: Simplify valid clap values

--- moss/src/cli/search.rs
+++ moss/src/cli/search.rs
@@ -43,9 +43,8 @@ pub fn command() -> Command {
                 .long("provides")
                 .num_args(0..=1)
                 .require_equals(true)
-                .default_missing_value("binaries")
+                .default_missing_value("binary")
                 .value_parser([
-                    "binaries",
                     "library",
                     "name",
                     "soname",
@@ -66,6 +65,13 @@ pub fn command() -> Command {
 enum MatchKind {
     Name,
     Summary,
+}
+
+fn map_aliases(value: &str) -> &str {
+    match value {
+        "library" => "soname",
+        _ => value,
+    }
 }
 
 pub fn handle(args: &ArgMatches, installation: Installation) -> Result<(), Error> {
