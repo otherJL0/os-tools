6cbd1ede2efe22fc53c43762fb8e0c72abcdecaf
Joey Riches <josephriches@gmail.com>
2026-04-02 10:01:32 +0100

    moss/cli: Convert install() to use clap derive structs

--- moss/src/cli/install.rs
+++ moss/src/cli/install.rs
@@ -3,45 +3,48 @@
 
 use std::path::PathBuf;
 
-use clap::{ArgMatches, Command, arg, value_parser};
+use clap::{ArgMatches, CommandFactory, FromArgMatches, Parser};
+
 use moss::{Installation, client::Client, environment};
 use tracing::instrument;
 
 pub use moss::client::Error;
 
-pub fn command() -> Command {
-    Command::new("install")
-        .visible_alias("it")
-        .about("Install packages")
-        .long_about("Install the requested software to the local system")
-        .arg(arg!(<NAME> ... "packages to install").value_parser(value_parser!(String)))
-        .arg(
-            arg!(--to <blit_target> "Blit this install to the provided directory instead of the root")
-                .help(
-                    "Blit this install to the provided directory instead of the root. \n\
-                     \n\
-                     This operation won't be captured as a new state",
-                )
-                .value_parser(value_parser!(PathBuf)),
-        )
+pub fn command() -> clap::Command {
+    Command::command()
+}
+
+#[derive(Debug, Parser)]
+#[command(
+    name = "install",
+    visible_alias = "it",
+    about = "Install packages",
+    long_about = "Install packages by name"
+)]
+pub struct Command {
+    /// Packages to install
+    packages: Vec<String>,
+
+    /// Blit this sync to the provided directory instead of the root
+    ///
+    /// This operation won't be captured as a new state
+    #[arg(value_name = "dir", long = "to")]
+    blit_target: Option<PathBuf>,
 }
 
 /// Handle execution of `moss install`
 #[instrument(skip_all)]
 pub fn handle(args: &ArgMatches, installation: Installation) -> Result<(), Error> {
-    let pkgs = args
-        .get_many::<String>("NAME")
-        .into_iter()
-        .flatten()
-        .map(String::as_str)
-        .collect::<Vec<_>>();
+    let command = Command::from_arg_matches(args).expect("validated by clap");
+
+    let pkgs = command.packages.iter().map(String::as_str).collect::<Vec<_>>();
     let yes = *args.get_one::<bool>("yes").unwrap();
 
     // Grab a client for the root
     let mut client = Client::new(environment::NAME, installation)?;
 
     // Make ephemeral if a blit target was provided
-    if let Some(blit_target) = args.get_one::<PathBuf>("to").cloned() {
+    if let Some(blit_target) = command.blit_target {
         client = client.ephemeral(blit_target)?;
     }
 
